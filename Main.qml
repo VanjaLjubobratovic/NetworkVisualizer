1f9b8ad3e886be400495c3f62e22e2157babc6b2
Vanja Ljubobratovic <vanja1708@gmail.com>
2023-05-18 17:18:40 +0200

    Initial GraphView setting and cpp QML connection

--- Main.qml
+++ Main.qml
@@ -1,9 +1,23 @@
 import QtQuick
-import QtQuick.Window
+import QtQuick.Window 2.3
+import QtQuick.Controls 2.5
+import QtQuick.Controls.Material 2.3
+import QuickQanava 2.0 as Qan
+import "qrc:/QuickQanava" as Qan
 
 Window {
-	width: 640
-	height: 480
+	width: 1280
+	height: 720
 	visible: true
-	title: qsTr("Hello World")
+	title: qsTr("Network visualizer")
+
+	Qan.GraphView {
+		id: graphView
+		anchors.fill: parent
+		navigable: true
+		graph: Qan.Graph {
+			objectName: "graph"
+			anchors.fill: parent
+		}
+	}
 }
--- main.cpp
+++ main.cpp
@@ -3,6 +3,19 @@
 #include <QuickQanava>
 #include <QQuickStyle>
 
+//Function for accessing Qan.Graph element in QML
+QPointer<qan::Graph> findGraph(QQmlApplicationEngine* engine) {
+	QPointer<qan::Graph> graph;
+	for (const auto rootObject : engine->rootObjects()) {
+		graph = qobject_cast<qan::Graph*>(rootObject->findChild<QQuickItem *>("graph"));
+		if(graph) {
+			qDebug() << "Graph element found" << graph;
+			break;
+		}
+	}
+	return graph;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -21,6 +34,12 @@ int main(int argc, char *argv[])
 	QuickQanava::initialize(&engine);
 
 	engine.load(url);
+
+	QPointer<qan::Graph> graph = findGraph(&engine);
+	if(!graph) {
+		qDebug() << "Graph element not found!";
+		emit engine.quit();
+	}
 
 	return app.exec();
 }
