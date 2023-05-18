0a05eb330f1cdf33b59d96e7aa70e1f5d711853d
Vanja Ljubobratovic <vanja1708@gmail.com>
2023-05-18 17:11:55 +0200

    Added and initialized QuickQanava

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QuickQanava>
+#include <QQuickStyle>
 
 
 int main(int argc, char *argv[])
 {
 	QGuiApplication app(argc, argv);
 
+	/*This in tandem with qtquickcontrols2.conf
+	 fixes invisible node text due to darkmode linux*/
+	QQuickStyle::setStyle("Material");
+
 	QQmlApplicationEngine engine;
 	const QUrl url(u"qrc:/NetworkVisualizer/Main.qml"_qs);
 	QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
 		&app, []() { QCoreApplication::exit(-1); },
 		Qt::QueuedConnection);
+
+	QuickQanava::initialize(&engine);
+
 	engine.load(url);
 
 	return app.exec();
